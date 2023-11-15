6febcd16a42fc8ac55e2ded5c7b9ca89496ea77d
Paulo Sergio Dos Santos Silva <paulo.santossilva@gmail.com>
2023-11-15 12:34:50 +0000

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 09:32:32 by pauldos-          #+#    #+#             */
-/*   Updated: 2023/11/14 16:13:02 by pauldos-         ###   ########.fr       */
+/*   Updated: 2023/11/15 12:29:25 by pauldos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,61 @@ int	ft_putnbr(int nb, int i)
 	return (i);
 }
 
+int	ft_putnbr_u(unsigned int nb, int i)
+{
+	int long	num;
+
+	num = nb;
+	if (nb < 10)
+		i = ft_putchar(nb + '0');
+	else {
+		i = ft_putnbr_u(nb / 10, i);
+		i = ft_putnbr_u(nb % 10, i);
+	}
+	return (i);
+}
+
+/*int	ft_putnbr_hex2(unsigned long long nb, int i, int uppercase)
+{
+	if (nb == 0)
+		i += ft_putchar('0');
+	else if (nb < 16)
+	{
+		if (nb < 10)
+			i += ft_putchar(nb + '0');
+		else
+			i += ft_putchar(nb - 10 + (uppercase ? 'A' : 'a'));
+	}
+	else
+	{
+		i += ft_putnbr_hex(nb / 16, i, uppercase);
+		i += ft_putnbr_hex(nb % 16, i, uppercase);
+	}
+	return i;
+}*/
+
+int ft_putnbr_hex(unsigned long long nb, int i, int uppercase)
+{
+    if (nb == 0)
+    {
+        i += ft_putchar('0');
+    }
+    else
+    {
+        int skip_zeros = 1; // Flag to skip leading zeros
+        for (int j = 28; j >= 0; j -= 4)
+        {
+            int hex_digit = (nb >> j) & 0xF;
+            if (hex_digit != 0 || !skip_zeros)
+            {
+                skip_zeros = 0; // Skip leading zeros only
+                i += ft_putchar(hex_digit < 10 ? hex_digit + '0' : hex_digit - 10 + (uppercase ? 'A' : 'a'));
+            }
+        }
+    }
+    return i;
+}
+
 char	*ft_strchr(const char *str, int c)
 {
 	while (*str)
@@ -81,6 +136,15 @@ char	*ft_strchr(const char *str, int c)
 		str++;
 	}
 	return (0);
+}
+
+int	ft_putptr(void *ptr, int i)
+{
+	unsigned long long addr = (unsigned long long)ptr;
+
+	i += ft_putstr("0x");
+	i += ft_putnbr_hex(addr, i, 1);
+	return (i);
 }
 
 int	ft_printf_format(const char *format, int i, va_list args)
@@ -93,6 +157,14 @@ int	ft_printf_format(const char *format, int i, va_list args)
 		return (ft_putstr(va_arg(args, char *)));
 	else if (format[i + 1] == 'i' || format[i + 1] == 'd')
 		return (ft_putnbr(va_arg(args, int), i));
+	else if (format[i + 1] == 'u')
+		return (ft_putnbr_u(va_arg(args, unsigned int), i));
+	else if (format[i + 1] == 'p')
+		return (ft_putptr(va_arg(args, void *), i));
+	else if (format[i + 1] == 'x')
+		return ft_putnbr_hex(va_arg(args, unsigned long long), i, 0);
+	else if (format[i + 1] == 'X')
+		return ft_putnbr_hex(va_arg(args, unsigned long long), i, 1);
 	return (0);
 }
 
@@ -128,11 +200,15 @@ int	main(void)
 	char	c;
 	char	*str;
 	int		num;
-
+	unsigned int		num2;
+	void	*ptr;
+	
 	c = 'd';
 	str = "is Luka";
 	num = -2147483648;
-	ft_printf("My name %s! Character: %c. Number: %d\n", str, c, num);
-	printf("My name %s! Character: %c. Number: %d\n", str, c, num);
+	num2 = 300;
+	ptr = (void *)0x12345678;
+	ft_printf("My name %s! Character: %c. Number (d): %d (i): %i (u): %u. Pointer: %p. Hex %x | %X\n", str, c, num, num, num2, ptr, num2, num2);
+	printf("My name %s! Character: %c. Number (d): %d (i): %i (u): %u. Pointer: %p. Hex %x | %X\n", str, c, num, num, num2, ptr, num2, num2);
 	return (0);
 }
